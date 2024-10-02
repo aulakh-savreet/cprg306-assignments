2543c1ca28b47188394709f6ec662781ed25643f
aulakh-savreet <147961994+aulakh-savreet@users.noreply.github.com>
2024-10-01 22:46:37 -0600

    Create student-info.js

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Savreet Singh</p>
+      <p>
+        <Link href="https://github.com/aulakh-savreet">
+          GitHub Repository
+        </Link>
+      </p>
+    </div>
+  );
+}
